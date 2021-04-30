67ae3f3d8ce550017e236bac019e08b9b5d67ae8
andyliang <31957845+andyliangtw@users.noreply.github.com>
2021-04-30 18:10:21 +0800

    Add ModelForm

--- vendor/urls.py
+++ vendor/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.vendor_index, name="vendor_index"),
+    path('', views.index, name="index"),
+    path('create/', views.create_view),
 ]
--- vendor/views.py
+++ vendor/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+
 from .models import Vendor
+from .forms import VendorForm
 
 
-def vendor_index(request):
+def index(request):
     vendor_list = Vendor.objects.all()  # 把所有 Vendor 的資料取出來
     context = {'vendor_list': vendor_list}  # 建立 Dict 對應到 Vendor 的資料，
     return render(request, 'vendor/detail.html', context)
+
+
+# 針對 create.html
+def create_view(request):
+    form = VendorForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = VendorForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "vendor/create.html", context)
