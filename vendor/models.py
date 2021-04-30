0b7020823679f9d470de477e08c8239a6dc85874
andyliang <31957845+andyliangtw@users.noreply.github.com>
2021-04-30 16:41:29 +0800

    More ModelAdmin membersMore ModelAdmin members

--- vendor/models.py
+++ vendor/models.py
@@ -54,3 +54,7 @@ class Morethanfifty(admin.SimpleListFilter):
 class FoodAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Food._meta.fields]
     list_filter = [Morethanfifty]
+    fileds = ['price_name']  # 限制 Admin 可以修改的欄位
+    search_fields = ['food_name', 'price_name']  # 搜尋欄位
+    ordering = ['price_name']  # 價格 由小到大 排序
+    # ordering = ['-price_name']  # 價格 由大到小 排序
