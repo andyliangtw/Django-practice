e02736b6e9ea6a5f404d5ccfa0f289269fff7084
andyliang <31957845+andyliangtw@users.noreply.github.com>
2021-04-30 16:25:02 +0800

    Display Name and ID

--- vendor/models.py
+++ vendor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
@@ -9,9 +10,19 @@ class Vendor(models.Model):
     phone_number = models.CharField(max_length=20)  # 攤販的電話號碼
     address = models.CharField(max_length=100)  # 攤販的地址
 
+    def __str__(self):
+        return self.vendor_name
+
 
 class Food(models.Model):
     food_name = models.CharField(max_length=30)  # 食物名稱
     price_name = models.DecimalField(max_digits=3, decimal_places=0)  # 食物價錢
     food_vendor = models.ForeignKey(
         Vendor, on_delete=models.CASCADE)  # 代表這食物是由哪一個攤販所做的
+
+    def __str__(self):
+        return self.food_name
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'vendor_name')
